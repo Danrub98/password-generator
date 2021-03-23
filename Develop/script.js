2a6378d19f4bc52836e3e8426267df6b22cd8de5
Danrub98 <jrm.daniel02@gmail.com>
2021-03-23 17:41:22 -0600

    The code was modified to make it more simplier, only the Java was changed

--- Develop/script.js
+++ Develop/script.js
@@ -2,17 +2,6 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-var pwlenght = prompt ("How many characters does the password needs to have?");
-
-while (pwlenght < 8 || pwlenght >128) {
-  pwlenght = prompt ("Please select a range between 8 and 128 characters");
-};
-
-var upperCase = confirm ("Would you like it to have upper case?");
-var lowerCase = confirm ("Would you like it to have lower case?");
-var numbers = confirm ("Would you like it to have numbers");
-var symbols = confirm ("Would you like it to have simbols?")
-
 function generateRandomLower () {
 const lowerLetters = "qwertyuiopasdfghjklñzxcvbnm"
 return lowerLetters[Math.floor(Math.random() * lowerLetters.length)]
@@ -33,26 +22,73 @@ const number = "123456789"
 return number[Math.floor(Math.random() * number.length)]
 }
 
-function writePassword() {
+function generatePassword() {
   var password = "";
   var result = 0;
-  while ( result < pwlenght) {
-    if (upperCase) {password += generateRandomUpper(); 
-    result++;
-    };
-    if (lowerCase) {password += generateRandomLower();
-    result++;
-  };
-    if (numbers) {password += generateRandomNumber();
-    result++;
-    };
-    if (symbols) {password += generateRandomSymbol();
-    result++;
-    };
-  }
-  const finalpassword = password.slice(0, pwlenght);
-  return finalpassword
+  var pwlenght = prompt ("How many characters does the password needs to have?");
+
+while (pwlenght < 8 || pwlenght >128) {
+  pwlenght = prompt ("Please select a range between 8 and 128 characters");
 };
+
+var finaloptions = "";
+
+var upperCase = confirm ("Would you like it to have upper case?"); 
+if (upperCase) {password += generateRandomUpper();
+  finaloptions += "QWERTYUIOPASDFGHJKLÑZXCVBNM";
+}
+var lowerCase = confirm ("Would you like it to have lower case?");
+if (lowerCase) {password += generateRandomLower();
+  finaloptions += "qwertyuiopasdfghjklñzxcvbnm";
+}
+var numbers = confirm ("Would you like it to have numbers");
+if (numbers) {password += generateRandomNumber();
+  finaloptions += "123456789";
+}
+var symbols = confirm ("Would you like it to have simbols?");
+if (symbols) {password += generateRandomSymbol();
+  finaloptions += "!·$%&/()=?¿";
+}
+
+console.log (password);
+for (var i = password.length ; i < pwlenght ; i++) {
+  password += finaloptions [Math.floor(Math.random() * finaloptions.length)]
+}
+return password;
+};
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector('#password');
+  passwordText.value = password;
+}
+// Add event listener to generate button
+
+generateBtn.addEventListener('click', writePassword);
+
+
+// This was my first attempt to make the homework, the code works and can be used, but my Tutor helped me to make it more simple adding the "for";
+ // while ( result < pwlenght) {
+   // if (upperCase) {password += generateRandomUpper(); 
+    //result++;
+    //};
+    //if (lowerCase) {password += generateRandomLower();
+    //result++;
+  //};
+    //if (numbers) {password += generateRandomNumber();
+    //result++;
+    //};
+    //if (symbols) {password += generateRandomSymbol();
+    //result++;
+    //};
+  //}
+  //const finalpassword = password.slice(0, pwlenght);
+  //return finalpassword
+
+
+
+// This was some code I used to create different passwords just by clicking the bottom, but my Tutor told me that the assigment was to make the survey to the
+// user, in case they wanted to make a different password, changing the variables. But the code works and can be used in case is needed.
+
 
   //generatePassword();
   //var passwordText = document.querySelector("#password");
@@ -60,15 +96,20 @@ function writePassword() {
   //passwordText.value = password;
 
 // Add event listener to generate button
-document.getElementById('password').innerText = ("Your password is " + writePassword())
+//document.getElementById('password').innerText = ("Your password is " + writePassword())
 
-document.getElementById('generate').addEventListener( 'click', () => {
-  writePassword()
-  console.log (writePassword())
-});
+//document.getElementById('generate').addEventListener( 'click', () => {
+  //writePassword()
+  //console.log (writePassword())
+//});
 
-document.getElementById('generate').addEventListener( 'click', () => {
-  document.getElementById('password').innerText = ("Your password is " + writePassword())
-});
+//document.getElementById('generate').addEventListener( 'click', () => {
+  //document.getElementById('password').innerText = ("Your password is " + writePassword())
+//});
 
-console.log (writePassword());
+//console.log (writePassword());
+
+
+
+// Get references to the #generate element
+// Write password to the #password input
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # password-generator
+
+The password generator code was created to assist the User to create a secure password. To be able to use it, the User needs to select some criteria, like the password length, if it needs to have Uppercase, Lowercase, Numbers or Symbols. The program saves those decision in variables, and then make it match with the lenght of the password, once it has all the characters, they randomize and the user get's the password in the text box of the program.
+
+![Image of the project created](./Develop/PasswordImage.png)
+
+This is a first prototype, the idea is to add a bottom to save the password created in the clipboard, to make it more simple to the user to save the characters.
+
+In the final parts of the code, some parts are commented, this is funcional code that can be added, this was my first attemp before my Tutor help me to make it more simple, the "While" statement was replaced with a "For", and the Event listener was commented, because when it was clicked, a different password was created with the same criteria, but with different characters. Since this was not what the assigment requested, it was changed to make the questions again to create a different password.
